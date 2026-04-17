742060078c946b1f6d4d24edc583366acf1959ba
Duan Yutong <duan.yutong@calendly.com>
2026-04-17 17:37:27 -0400

    feat: add draft option to config and cli

--- github.go
+++ github.go
@@ -85,10 +85,17 @@ func githubCreatePRForCommit(commit *Commit, prev *Commit) error {
 		base = prev.GetRemoteRef()
 	}
 	args := []string{"pr", "create", "--title", commit.Title, "--body", "", "--head", commit.GetRemoteRef(), "--base", base}
+	if config.draft {
+		args = append(args, "--draft")
+	}
 	if tags := commit.GetTags(config.tags...); len(tags) > 0 {
 		args = append(args, "--label", strings.Join(tags, ","))
 	}
-	printf("create pull request for %q\n", commit.Title)
+	if config.draft {
+		printf("create draft pull request for %q\n", commit.Title)
+	} else {
+		printf("create pull request for %q\n", commit.Title)
+	}
 	_, err := gh(args...)
 	return err
 }
